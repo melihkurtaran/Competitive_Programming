c4edfd00f77c1e8c3a8bb49f34fedf7e0a0742a5
Melih Kurtaran <35525914+melihkurtaran@users.noreply.github.com>
2019-12-30 18:40:29 +0300

    update indentation

--- available-captures-for-rook/available-captures-for-rook.py
+++ available-captures-for-rook/available-captures-for-rook.py
@@ -1,34 +1,34 @@
 class Solution(object):
-def numRookCaptures(self, board):
-    output = 0
-    for i in range(0,len(board)):
-        for j in range(0,len(board[i])):
-            if board[i][j] == 'R':
-                for r in range(i-1,-1,-1):
-                    if board[r][j] == 'B':
-                        break
-                    if board[r][j] == 'p':
-                        output += 1
-                        break
-                for r in range(i+1,len(board)):
-                    if board[r][j] == 'B':
-                        break
-                    if board[r][j] == 'p':
-                        output += 1
-                        break
-                for c in range(j-1,-1,-1):
-                    if board[i][c] == 'B':
-                        break
-                    if board[i][c] == 'p':
-                        output += 1
-                        break
-                for c in range(j+1,len(board[i])):
-                    if board[i][c] == 'B':
-                        break
-                    if board[i][c] == 'p':
-                        output += 1
-                        break
-    return output
-                
-                
-    
+    def numRookCaptures(self, board):
+        output = 0
+        for i in range(0,len(board)):
+            for j in range(0,len(board[i])):
+                if board[i][j] == 'R':
+                    for r in range(i-1,-1,-1):
+                        if board[r][j] == 'B':
+                            break
+                        if board[r][j] == 'p':
+                            output += 1
+                            break
+                    for r in range(i+1,len(board)):
+                        if board[r][j] == 'B':
+                            break
+                        if board[r][j] == 'p':
+                            output += 1
+                            break
+                    for c in range(j-1,-1,-1):
+                        if board[i][c] == 'B':
+                            break
+                        if board[i][c] == 'p':
+                            output += 1
+                            break
+                    for c in range(j+1,len(board[i])):
+                        if board[i][c] == 'B':
+                            break
+                        if board[i][c] == 'p':
+                            output += 1
+                            break
+        return output
+
+
+
